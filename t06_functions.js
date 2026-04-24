5612e1138c481cdea1574aef7578b498f6bf60d0
23489ac <23489ac@hvhs.school.nz>
2026-04-24 00:11:00 +0000

    Apr 24, 2026, 12:11 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,40 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of the task:
+****************************/
+console.log("Running T5intro.js");
+
+//Variable
+let userName = 'Aarti';
+let userAge = 16;
+let usermoney = 10;
+let usercurrentyear = 2026;
+console.log (" hi "+ userName);
+console.log(" A of " + usercurrentyear + " you are "+ userAge +" years old ");
+console.log(" You have "+ usermoney +" dollars ");
+var mybirthyear = 2009
+var in10years = 10
+var youspend = 2
+var add = 3
+var leftmoney = 5
+console.log(" You were born in "+ mybirthyear);
+console.log(" In 10 year you will be ",+ userAge + in10years );
+console.log(" You spend half of your money,now you have ",+ usermoney / youspend);
+console.log(" Then you get $3, now you " ,+leftmoney + add  )
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+/****************************
+Main code
+****************************/
+OUTPUT.innerHTML ="<h2>spaceForJavaScriptOutput </h2> ";
+OUTPUT.innerHTML +="<p> Hi my name is Aarti </p> ";
+OUTPUT.innerHTML +="<p> i am 16 year old  </p> ";
+OUTPUT.innerHTML +="<p> I am studying cs </p> ";
+
+
+
+
+/****************************
+Functions
+****************************/
