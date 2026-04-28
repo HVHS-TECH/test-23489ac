adf651cf83487551b2602411d7039519796b67f9
23489ac <23489ac@hvhs.school.nz>
2026-04-28 03:15:29 +0000

    Apr 28, 2026, 3:15 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -12,17 +12,17 @@ let userAge = 16;
 let usermoney = 10;
 let usercurrentyear = 2026;
 var mybirthyear = 2009;
-var in10years = 10;
+let in10years = 10;
 var youspend = 2;
 var add = 3;
-var leftmoney = 5;
+let leftmoney = 5;
 OUTPUT.innerHTML = "<h1> hi "+ userName+ "</h1>";
 OUTPUT.innerHTML+= "<p>  As of " + usercurrentyear + " you are "+ userAge +" years old </p>";
 OUTPUT.innerHTML+="<p>  You have" + usermoney + " dollars </p>";
 OUTPUT.innerHTML+="<p>  You were born in " + mybirthyear+ "</p>"; 
 OUTPUT.innerHTML+="<p>  In 10 year you will be "+ userAge+in10years + "</p>";  
-OUTPUT.innerHTML+="<p> You spend half of your money,now you have " + usermoney/youspend + "</p>"; 
-OUTPUT.innerHTML+="<p>  Then you get $3, now you " , + leftmoney+add +"</p>"; 
+OUTPUT.innerHTML+="<p> You spend half of your money,now you have " + (usermoney/youspend) + "</p>"; 
+OUTPUT.innerHTML+="<p>  Then you get $3, now you " + leftmoney+add +"</p>"; 
 
 /****************************
 Main code
