7ba60f0e4204809b2c5db6b87313012dd72d195e
23489ac <23489ac@hvhs.school.nz>
2026-04-29 00:28:51 +0000

    Apr 29, 2026, 12:28 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -5,7 +5,6 @@ console.log("Running T5intro.js");
 
 //Variable
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-writeLine()
 function writeLine(){
 let userName = "Aarti";
 let userAge = 16;
@@ -13,17 +12,18 @@ let usercurrentyear = 2026;
 var usermoney = 10;
 var mybirthyear = 2009;
 let in10years = 10;
-var youspend = 2; f 
+var youspend = 2; 
 var add = 3;
 let leftmoney = 5;
+
 OUTPUT.innerHTML = "<h1> hi "+ userName+ "</h1>";
 OUTPUT.innerHTML+= "<p>  As of " + usercurrentyear + " you are "+ userAge +" years old </p>";
 OUTPUT.innerHTML+="<p>  You have" + usermoney + " dollars </p>";
 OUTPUT.innerHTML+="<p>  You were born in " + mybirthyear+ "</p>"; 
-OUTPUT.innerHTML+="<p>  In 10 year you will be "+ userAge+in10years + "</p>";  
+OUTPUT.innerHTML+="<p>  In 10 year you will be "+ (userAge+in10years) + "</p>";  
 let spent = usermoney/youspend;
-OUTPUT.innerHTML+="<p> You spend half of your money,now you have " + spent + "</p>"; 
-OUTPUT.innerHTML+="<p>  Then you get $3, now you " + leftmoney+add +"</p>"; 
+OUTPUT.innerHTML+="<p> You spend half of your money,now you have " +spent+ "</p>"; 
+OUTPUT.innerHTML+="<p>  Then you get $3, now you " + (leftmoney+add) +"</p>"; 
 
 /****************************
 Main code
