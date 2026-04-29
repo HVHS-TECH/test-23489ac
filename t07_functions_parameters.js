9e7591d1e183cd74ff1b9be3a789cc453a1d7e39
23489ac <23489ac@hvhs.school.nz>
2026-04-29 21:56:18 +0000

    Apr 29, 2026, 9:56 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -43,8 +43,8 @@ function writeLine(){
 }
 displayProduct("Cholate bar, 4");
 displayProduct("Chips, 3");
-displayProduct(",Drink 2.50");
+displayProduct("Drink ,2.50");
 
 function displayProduct(_name,_price){
-    OUTPUT.innerHTML+= "<p>" +_name '+: $ '+ _price + "</p>";
+    OUTPUT.innerHTML+= "<p>" +_name +": $" + _price + "</p>";
 }
