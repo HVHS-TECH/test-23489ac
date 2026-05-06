e0946f005fc7e5dca319c34e681877bd4d8bf662
23489ac <23489ac@hvhs.school.nz>
2026-05-06 05:20:16 +0000

    May 6, 2026, 5:20 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -51,10 +51,11 @@ function getFormInput(){
 function getFormInput(){
  const AGE_fIELD = document.getElementById("ageField");
     let userAge = AGE_fIELD.value;
-    OUTPUT.innerHTML= "<p> Your age is :" +userAge+ "</p>"
 const MONEY_fIELD = document.getElementById("moneyField");
     let usermoney = MONEY_fIELD.value;
+    OUTPUT.innerHTML= "<p> Your age is :" +userAge+ "</p>"
     OUTPUT.innerHTML= "<p> Your pocket money is: $ " +usermoney+ "</p>"
+    OUTPUT.innerHTML= "<p> Your age is :" +userAge+ "</p>"
 if (userAge>=16 ){
     OUTPUT.innerHTML+="<p>you are old enough to work </p>";    
 } else{
