bc904eb85de81e67951439bc69c150408e7afedb
23489ac <23489ac@hvhs.school.nz>
2026-05-13 08:16:19 +0000

    May 13, 2026, 8:16 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -8,14 +8,17 @@ let userName = 'Aarti';
 let userAge = 16;
 let usermoney = 10;
 let usercurrentyear = 2026;
+
 console.log (" hi "+ userName);
 console.log(" A of " + usercurrentyear + " you are "+ userAge +" years old ");
 console.log(" You have "+ usermoney +" dollars ");
+
 var mybirthyear = 2009
 var in10years = 10
 var youspend = 2
 var add = 3
 var leftmoney = 5
+
 console.log(" You were born in "+ mybirthyear);
 console.log(" In 10 year you will be ",+ userAge + in10years );
 console.log(" You spend half of your money,now you have ",+ usermoney / youspend);
@@ -43,13 +46,12 @@ function getFormInput(){
     let userName = nameField.value;
 
     OUTPUT.innerHTML= "<p> welcome to the page "+ userName +"</p>"
-    OUTPUT.innerHTML +="<h2>spaceForJavaScriptOutput </h2> ";
     OUTPUT.innerHTML +="<p> Hi my name is Aarti </p> ";
     OUTPUT.innerHTML +="<p> i am 16 year old  </p> ";
     OUTPUT.innerHTML +="<p> I am studying cs </p> ";
 
 writeLine();
-displayProduct("Cholate bar", 4);
+displayProduct("Chocolate bar", 4);
 displayProduct("Chips", 3);
 displayProduct("Drink ",2.50);
 }
