c906e7a20d82269f1e29e4db8bf4a41b691ca66d
23489ac <23489ac@hvhs.school.nz>
2026-05-18 00:53:50 +0000

    May 18, 2026, 12:53 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -8,7 +8,7 @@ console.log("Running T15_iteration_for_loop.js");
 /****************************
 Main code
 ****************************/
- const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 
 
@@ -16,21 +16,21 @@ Main code
 Functions
 ****************************/
 function startSong() {
-const VERSE_FIELD = document.getElementById("verse_Field");
-let verses = Number(VERSE_FIELD.value);
-OUTPUT.innerHTML = ""; 
-for (let bottles = verses; bottles >= 1; bottles--) {
-if (bottles === 1) {
-OUTPUT.innerHTML += "<p>1 bottle of milk on the wall, 1 bottle of milk.</p>";
-OUTPUT.innerHTML += "<p>Take one down and pass it around, no more bottles of milk on the wall.</p>";
-} else {
-OUTPUT.innerHTML += "<p>" + bottles + " bottles of milk on the wall, " + bottles + " bottles of milk.</p>";
-OUTPUT.innerHTML += "<p>Take one down and pass it around, " + (bottles + 1);
- if (bottles -1 === 1) {
-  OUTPUT.innerHTML += " bottle of milk on the wall.</p>";
-} else {
-  OUTPUT.innerHTML += " bottles of milk on the wall.</p>";
- }
+  const VERSE_FIELD = document.getElementById("verse_Field");
+  let verses = Number(VERSE_FIELD.value);
+  OUTPUT.innerHTML = "";
+  for (let bottles = verses; bottles >= 1; bottles--) {
+    if (bottles === 1) {
+      OUTPUT.innerHTML += "<p>1 bottle of milk on the wall, 1 bottle of milk.</p>";
+      OUTPUT.innerHTML += "<p>Take one down and pass it around, no more bottles of milk on the wall.</p>";
+    } else {
+      OUTPUT.innerHTML += "<p>" + bottles + " bottles of milk on the wall, " + bottles + " bottles of milk.</p>";
+      OUTPUT.innerHTML += "<p>Take one down and pass it around, " + (bottles + 1);
+      if (bottles - 1 === 1) {
+        OUTPUT.innerHTML += " bottle of milk on the wall.</p>";
+      } else {
+        OUTPUT.innerHTML += " bottles of milk on the wall.</p>";
+      }
     }
 
   }
