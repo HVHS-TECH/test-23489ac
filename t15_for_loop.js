6544bc1738c6298b7851ddab1beb9970d73130c9
23489ac <23489ac@hvhs.school.nz>
2026-05-20 21:14:15 +0000

    May 20, 2026, 9:14 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -41,12 +41,11 @@ function startSong() {
   if (i<=1)  {
  OUTPUT.innerHTML +="<p>" +i+  "banana </p>"
 } 
-}else{ 
-OUTPUT.innerHTML +="<p> All the bananas </p>"
+} else { 
 OUTPUT.innerHTML +="<p>" +i+  "bananas </p>"
+OUTPUT.innerHTML +="<p> All the bananas </p>"
 }
 
   
 
   }
-}
