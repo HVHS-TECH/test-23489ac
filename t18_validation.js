bd25d9881b7c8a296db5da936dff2962d67534bc
23489ac <23489ac@hvhs.school.nz>
2026-05-20 22:51:12 +0000

    May 20, 2026, 10:51 PM

--- t18_validation.js
+++ t18_validation.js
@@ -93,16 +93,18 @@ function addItem(){
   const itemField = document.getElementById("itemField");
   if( itemField.checkvalidity()=== false){
     OUTPUT.innerHTML = "please fill out all fiels corretly.<br>";
-  }
+  } else{
   let userItem = itemField.value;
   shoppingList.push("userItem")
   OUTPUT.innerHTML += "<p>You have added "+ userItem +" to the list </p>";
-  
-
 }
+
+
 function showList() {
 OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
 for(let i= 0; i<shoppingList.length; i++ ){
  OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
+}
+}
 }
 }
