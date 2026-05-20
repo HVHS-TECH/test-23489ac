a59cc353c551a7f74e394e647da4becac5f4b47a
23489ac <23489ac@hvhs.school.nz>
2026-05-20 22:27:20 +0000

    May 20, 2026, 10:27 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -67,7 +67,7 @@ function getFormInput(){
  const PRICE = 4
  OUTPUT.innerHTML = "<p>A chocolate bar costs $4</p>";
     if (usermoney >=PRICE ){
-        var change = calculateChange (usermoney,PRICE )
+        let change = calculateChange (usermoney,PRICE )
     OUTPUT.innerHTML +="<p>You CAN afford a chocolate bar</p>";    
     OUTPUT.innerHTML += "<p>You will get $" + change + " change</p>";
 } else{
