a964c4c16ddb8629a9390b85cd9151c43ab6739c
23489ac <23489ac@hvhs.school.nz>
2026-05-22 00:21:48 +0000

    May 22, 2026, 12:21 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -19,13 +19,13 @@ function startSong() {
   const VERSE_FIELD = document.getElementById("verse_Field");
   let verses = Number(VERSE_FIELD.value);
   OUTPUT.innerHTML = "";
-  for (let bottles = verses; bottles <= 1; bottles--) {
+  for (let bottles = verses; bottles >= 1; bottles--) {
     if (bottles === 1) {
       OUTPUT.innerHTML += "<p>1 bottle of milk on the wall, 1 bottle of milk.</p>";
       OUTPUT.innerHTML += "<p>Take one down and pass it around, no more bottles of milk on the wall.</p>";
     } else {
       OUTPUT.innerHTML += "<p>" + bottles + " bottles of milk on the wall, " + bottles + " bottles of milk.</p>";
-      OUTPUT.innerHTML += "<p>Take one down and pass it around, " + (bottles + 1);
+      OUTPUT.innerHTML += "<p>Take one down and pass it around, " + (bottles - 1);
       if (bottles - 1 === 1) {
         OUTPUT.innerHTML += " bottle of milk on the wall.</p>";
       } else {
@@ -34,7 +34,7 @@ function startSong() {
     }
 
   }
-}
+
 
 function startmainSong() {
   for(let i= 1; i<10; i++ ){
@@ -45,4 +45,5 @@ OUTPUT.innerHTML +="<p>" +i+  "bananas </p>"
 }
   }
  OUTPUT.innerHTML +="<p> All the bananas </p>" 
+}
 }
