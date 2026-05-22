d3c6964a77e9ac195425e05385c41c6f2f0aa9d1
23489ac <23489ac@hvhs.school.nz>
2026-05-22 00:11:55 +0000

    May 22, 2026, 12:11 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -8,18 +8,18 @@ let userName = 'Aarti';
 let userAge = 16;
 let usermoney = 10;
 let usercurrentyear = 2026;
-console.log (" hi "+ userName);
-console.log(" A of " + usercurrentyear + " you are "+ userAge +" years old ");
-console.log(" You have "+ usermoney +" dollars ");
+console.log(" hi " + userName);
+console.log(" A of " + usercurrentyear + " you are " + userAge + " years old ");
+console.log(" You have " + usermoney + " dollars ");
 var mybirthyear = 2009
 var in10years = 10
 var youspend = 2
 var add = 3
 var leftmoney = 5
-console.log(" You were born in "+ mybirthyear);
-console.log(" In 10 year you will be ",+ userAge + in10years );
-console.log(" You spend half of your money,now you have ",+ usermoney / youspend);
-console.log(" Then you get $3, now you " ,+leftmoney + add  )
+console.log(" You were born in " + mybirthyear);
+console.log(" In 10 year you will be ", + userAge + in10years);
+console.log(" You spend half of your money,now you have ", + usermoney / youspend);
+console.log(" Then you get $3, now you ", +leftmoney + add)
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
@@ -27,71 +27,71 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 /****************************
 Main code
 ****************************/
-function start(){
+function start() {
 
-OUTPUT.innerHTML ="<h2>spaceForJavaScriptOutput </h2> ";
-OUTPUT.innerHTML +="<p> Hi my name is Aarti </p> ";
-OUTPUT.innerHTML +="<p> i am 16 year old  </p> ";
-OUTPUT.innerHTML +="<p> I am studying cs </p> ";
+    OUTPUT.innerHTML = "<h2>spaceForJavaScriptOutput </h2> ";
+    OUTPUT.innerHTML += "<p> Hi my name is Aarti </p> ";
+    OUTPUT.innerHTML += "<p> i am 16 year old  </p> ";
+    OUTPUT.innerHTML += "<p> I am studying cs </p> ";
 
 
-writeLine();
-/****************************
-Functions
-****************************/
-function writeLine(){
-    OUTPUT.innerHTML +="<p>Welcome to the shop</p>";
+    writeLine();
+    /****************************
+    Functions
+    ****************************/
+    function writeLine() {
+        OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+    }
 }
-}
-function getFormInput(){
-      alert("button works");
+function getFormInput() {
+    alert("button works");
     const NAME_fIELD = document.getElementById("nameField");
     let userName = NAME_fIELD.value;
-    OUTPUT.innerHTML+= "<p> welcome to my page "+userName+"</p>"
+    OUTPUT.innerHTML += "<p> welcome to my page " + userName + "</p>"
 }
-function getFormInput(){
- const AGE_fIELD = document.getElementById("ageField");
+function getFormInput() {
+    const AGE_fIELD = document.getElementById("ageField");
     let userAge = AGE_fIELD.value;
-const MONEY_fIELD = document.getElementById("moneyField");
-    let usermoney = MONEY_fIELD.value;
-
-    OUTPUT.innerHTML+= "<p> Your age is :" +userAge+ "</p>"
-    OUTPUT.innerHTML+= "<p> Your pocket money is: $ " +usermoney+ "</p>"
-}
-function calculateChange(_money,_price){
-    return _money - _price;
-}
-function getFormInput(){
     const MONEY_fIELD = document.getElementById("moneyField");
     let usermoney = MONEY_fIELD.value;
- const PRICE = 4
- OUTPUT.innerHTML = "<p>A chocolate bar costs $4</p>";
-    if (usermoney >=PRICE ){
-        var change = calculateChange (usermoney,PRICE )
-    OUTPUT.innerHTML +="<p>You CAN afford a chocolate bar</p>";    
-    OUTPUT.innerHTML += "<p>You will get $" + change + " change</p>";
-} else{
-    OUTPUT.innerHTML+="<p>Sorry you CAN'T afford a chocolate bar </p>"; 
+
+    OUTPUT.innerHTML += "<p> Your age is :" + userAge + "</p>"
+    OUTPUT.innerHTML += "<p> Your pocket money is: $ " + usermoney + "</p>"
 }
+function calculateChange(_money, _price) {
+    return _money - _price;
 }
-function getFormInput(){
-let messages = [ "You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER" ];
+function getFormInput() {
+    const MONEY_fIELD = document.getElementById("moneyField");
+    let usermoney = MONEY_fIELD.value;
+    const PRICE = 4
+    OUTPUT.innerHTML = "<p>A chocolate bar costs $4</p>";
+    if (usermoney >= PRICE) {
+        var change = calculateChange(usermoney, PRICE)
+        OUTPUT.innerHTML += "<p>You CAN afford a chocolate bar</p>";
+        OUTPUT.innerHTML += "<p>You will get $" + change + " change</p>";
+    } else {
+        OUTPUT.innerHTML += "<p>Sorry you CAN'T afford a chocolate bar </p>";
+    }
+}
+function getFormInput() {
+    let messages = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER"];
     const chocolate_Field = document.getElementById("chocolate_Field");
     let userChoice = chocolate_Field.value;
-    OUTPUT.innerHTML += "<p>"+ messages[userChoice]+ "</p>";
+    OUTPUT.innerHTML += "<p>" + messages[userChoice] + "</p>";
 }
 
 let shoppingList = [];
-function addItem(){
-  const itemField = document.getElementById("itemField");
-  let userItem = itemField.value;
-  shoppingList.push(userItem)
-  OUTPUT.innerHTML += "<p>You have added "+ userItem +" to the list </p>";
+function addItem() {
+    const itemField = document.getElementById("itemField");
+    let userItem = itemField.value;
+    shoppingList.push(userItem)
+    OUTPUT.innerHTML += "<p>You have added " + userItem + " to the list </p>";
 
 }
 function showList() {
-OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
-for(let i= 0; i<shoppingList.length; i++ ){
- OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
-}
+    OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
+    }
 }
