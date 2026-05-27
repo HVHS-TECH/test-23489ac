cd379c0d304a142b70a1f3c489ac2955bf7f751b
23489ac <23489ac@hvhs.school.nz>
2026-05-27 22:10:23 +0000

    May 27, 2026, 10:10 PM

--- t18_validation.js
+++ t18_validation.js
@@ -50,9 +50,9 @@ function getNameInput() {
         OUTPUT.innerHTML += "<p> welcome to my page " + userName + "</p>"
 } else if (userName.length < 3) {
      OUTPUT.innerHTML = "<p>please write the name .</p>";
-}else if {
+}else {
     OUTPUT.innerHTML = "<p>Welcome to my page"+ userName+".</p>";
-} else  (NaN(userName)) {
+} else (NaN(userName)) {
      OUTPUT.innerHTML = "<p> please write the name .</p>";
 }
 }
