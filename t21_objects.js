db4635a6fce6b392b34a37110533ef1ef4f5d24e
23489ac <23489ac@hvhs.school.nz>
2026-06-03 21:42:46 +0000

    Jun 3, 2026, 9:42 PM

--- t21_objects.js
+++ t21_objects.js
@@ -1,1 +1,114 @@
-/* Paste your code from the last task */
+/****************************
+Name of the task:
+****************************/
+console.log("Running T20_constants.js");
+
+//Variable
+const userName = 'Aarti';
+const userAge = 16;
+const usermoney = 10;
+const usercurrentyear = 2026;
+console.log(" hi " + userName);
+console.log(" A of " + usercurrentyear + " you are " + userAge + " years old ");
+console.log(" You have " + usermoney + " dollars ");
+const mybirthyear = 2009
+const in10years = 10
+const youspend = 2
+const add = 3
+const leftmoney = 5
+console.log(" You were born in " + mybirthyear);
+console.log(" In 10 year you will be ", + userAge + in10years);
+console.log(" You spend half of your money,now you have ", + usermoney / youspend);
+console.log(" Then you get $3, now you ", +leftmoney + add)
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+/****************************
+Main code
+****************************/
+function start() {
+
+    OUTPUT.innerHTML = "<h2>spaceForJavaScriptOutput </h2> ";
+    OUTPUT.innerHTML += "<p> Hi my name is Aarti </p> ";
+    OUTPUT.innerHTML += "<p> i am 16 year old  </p> ";
+    OUTPUT.innerHTML += "<p> I am studying cs </p> ";
+
+
+    writeLine();
+    /****************************
+    Functions
+    ****************************/
+    function writeLine() {
+        OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+    }
+}
+function getNameInput() {
+    const NAME_fIELD = document.getElementById("nameField");
+    let userName = NAME_fIELD.value;
+if (userName.length < 3) {
+     OUTPUT.innerHTML = "<p> The name must be at least three letters long.</p>";
+} else if (!userName) {
+     OUTPUT.innerHTML = "<p> Enter your name  .</p>";
+}else if (NAME_fIELD.checkValidity() === false){
+    OUTPUT.innerHTML += "<p>please fill all the fiels correctly </p>";
+}else {
+    OUTPUT.innerHTML += "<p> welcome to my page " + userName + "</p>";
+}
+}
+    
+function getAgeInput() {
+    const AGE_fIELD = document.getElementById("ageField");
+    let userAge = AGE_fIELD.value;
+    const MONEY_fIELD = document.getElementById("moneyField");
+    let usermoney = MONEY_fIELD.value;
+
+    OUTPUT.innerHTML += "<p> Your age is :" + userAge + "</p>"
+    OUTPUT.innerHTML += "<p> Your pocket money is: $ " + usermoney + "</p>"
+}
+function calculateChange(_money, _price) {
+    return _money - _price;
+}
+function getMoneyInput() {
+    const MONEY_fIELD = document.getElementById("moneyField");
+    let usermoney = MONEY_fIELD.value;
+    const PRICE = 4
+    OUTPUT.innerHTML = "<p>A chocolate bar costs $4</p>";
+    if (usermoney >= PRICE) {
+        var change = calculateChange(usermoney, PRICE)
+        OUTPUT.innerHTML += "<p>You CAN afford a chocolate bar</p>";
+        OUTPUT.innerHTML += "<p>You will get $" + change + " change</p>";
+    } else {
+        OUTPUT.innerHTML += "<p>Sorry you CAN'T afford a chocolate bar </p>";
+    }
+}
+function getMessageInput() {
+    let messages = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER"];
+    const chocolate_Field = document.getElementById("chocolate_Field");
+    let userChoice = chocolate_Field.value;
+    OUTPUT.innerHTML += "<p>" + messages[userChoice] + "</p>";
+}
+
+let shoppingList = [];
+function shoppingitem() {
+    const itemField = document.getElementById("itemField");
+    if (itemField.checkValidity() === false) {
+        OUTPUT.innerHTML = "please fill out all fiels corretly.<br>";
+    } else { 
+    let userItem = itemField.value;
+    shoppingList.push(userItem)
+    OUTPUT.innerHTML += "<p>You have added " + userItem + " to the list </p>";
+}
+}
+
+function shoppingshow() {
+    const itemField = document.getElementById("itemField");
+    OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
+    if (itemField.checkValidity() === false) {
+        OUTPUT.innerHTML = "please fill out all fields corretly.<br>";
+    } else { 
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
+    }
+}
+}
