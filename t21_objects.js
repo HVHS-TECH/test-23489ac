3c2fd7bf7c5e35548bcb958daac98d607f551d72
23489ac <23489ac@hvhs.school.nz>
2026-06-05 00:16:21 +0000

    Jun 5, 2026, 12:16 AM

--- t21_objects.js
+++ t21_objects.js
@@ -81,7 +81,7 @@ if ( !userName) {
 
     OUTPUT.innerHTML += "<h3> Welcome to all the Users:</h3>";
     for (let i = 0; i < users.length; i++) {4
-        OUTPUT.innerHTML += "<p>Hello " + users.name + "</p>";
+        OUTPUT.innerHTML += "<p>Hello " + userObject.name + "</p>";
     }
 }
 
